6d88702a9a9c2f6e6585596d99d24a9ac504c8f0
Faip Mardoni <faipmardoni@gmail.com>
2018-04-04 20:05:13 +0700

    refaktor code

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-function sortingArray(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for(let j = i+1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
-    }
-  }
-  return arr
-}
 function generateBoard(side) {
-  let num = side*side;
   let output = [];
-  if (side % 2 === 0) {
-    for (let row = 0; row < side; row++) {
-      output[row] = [];
-      for (let col = 0; col < side; col++) {
-        output[row].push(num);
-        num--;
+  let start = 1;
+
+  for (let row = side-1; row >= 0; row--) {
+    output[row] = [];
+    // if (side % 2 === 0) {
+      if ((row % 2 === 1 && side % 2 === 0) || (side % 2 === 1 && row % 2 === 0)) {
+        for (col = 0; col < side; col++) {
+          output[row][col] = start
+          start++
+        }
+      }else {
+        for (col = side-1; col >= 0; col--) {
+          output[row][col] = start;
+          start++
+        }
       }
-      if(row % 2 === 1) sortingArray(output[row])
-    }
-  }else {
-    for (let row = 0; row < side; row++) {
-      output[row] = [];
-      for (let col = 0; col < side; col++) {
-        output[row].push(num);
-        num--;
-      }
-      if(row % 2 === 0) sortingArray(output[row])
-    }
   }
   return output;
 }
 
-console.log(generateBoard(15));
+console.log(generateBoard(5));
+console.log("========")
+console.log(generateBoard(10));
 
 
